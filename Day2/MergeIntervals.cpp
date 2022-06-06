616749c74187ae18e42bc740a80a591f017a50d1
pawangoyal02 <goyalpawan892@gmail.com>
2022-06-06 12:36:01 +0530

    Day2

---
+++ Day2/MergeIntervals.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    // Write your code here.
+    sort(intervals.begin(), intervals.end());
+        vector<vector<int>> merged;
+        
+        for(int i=0; i<intervals.size(); i++) {
+            if(merged.empty() || merged.back()[1] < intervals[i][0]) {
+                vector<int> v= {intervals[i][0], intervals[i][1]};
+                merged.push_back(v);
+            }
+            else {
+                merged.back()[1]= max(merged.back()[1], intervals[i][1]);
+            }
+        }
+        return merged;
+}
